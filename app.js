b3d6f6a367a2a005afdbdb0ac4caa4c615c221bb
Nancy Schrader <katelanschrader7@gmail.com>
2020-02-02 20:22:45 -0600

    updating

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 $(() => {
+  $("form").on("submit", event => {
+    event.preventDefault();
+    //const userInput = $("#search-box").val()
+    const userInput = $('input[type="text"]').val();
+
+    $.ajax({
+      url: `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/10221145922013074/search/${userInput}` //${userInput}
+    }).then(
+      data => {
+        console.log(data);
+        var content = data.results[0].powerstats;
+        $("#powerstats").append(content);
+        //console.log(data);
+      },
+      () => {
+        console.log("bad request");
+      }
+    );
     
-  
-      $.ajax({
-        url: "https://superheroapi.com/api/10221145922013074/14/powerstats"
-      }).then(
-        (data) => {
-          console.log(data);
-        },
-        () => {
-          console.log("bad request");
-        }
-      );
   });
-  
+});
