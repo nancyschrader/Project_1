748fdfc6a7c1e59519762db24c497b264fdc20e4
Nancy Schrader <katelanschrader7@gmail.com>
2020-02-05 18:11:32 -0600

    code clean up and changed seachbox background to white

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(() => {
   $("form").on("submit", event => {
     event.preventDefault();
-    //const userInput = $("#search-box").val()
+    
     const userInput = $('input[type="text"]').val();
 
     $.ajax({
@@ -42,43 +42,3 @@ $(() => {
   });
 
 });
-// const nancyTest = () => {
-//   let $image = $("<div>").addClass("image");
-//   $(".wrapper").append($image);
-// };
-
-// let $image = $("<div>").addClass("image");
-// $('.wrapper').append($image);
-
-// // $('image').attr(results[0].image.url);
-// let $name = $("<div").addClass("name");
-// $("body").append($name);
-
-// let $information = $("<div>").addClass("information");
-// $("body").append($information);
-
-// let $powerstats = $("<div>").addClass("powerstats");
-// $("body").append($powerstats);
-
-// let this_image = $("image").attr('src', results[0].image.url);
-
-// $('body').append(this_image)
-
-// var image = '${results[0].image.url}';
-
-// var information = '${results[0].biography}';
-
-// var powerstats = '${results[0].powerstats}';
-// var name = '${results[0].name}';
-
-// $('.image').html(image);
-// $('.name').html(text);
-// $('.information').html(text);
-// $('.powerstats').html(text);
-
-// $("#results").append(name);
-//$("#result").html(data);
-
-// var content = data.results[0].powerstats;
-// $("#powerstats").append(content);
-//console.log(data);
